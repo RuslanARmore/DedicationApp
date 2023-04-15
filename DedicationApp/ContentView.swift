5275fc4f81893d0463243ba9eeb2b3fe90efb6ec
Ruslan Akhriyev <ruslan.a@ati.su>
2023-04-15 22:58:27 +0800

    init commit

---
+++ DedicationApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DedicationApp
+//
+//  Created by Ruslan Akhriev on 15.04.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DedicationApp/DedicationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DedicationAppApp.swift
+//  DedicationApp
+//
+//  Created by Ruslan Akhriev on 15.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct DedicationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
